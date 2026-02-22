c49d9b1c36b22f9f78fec59a008b5503f489c1e3
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-02-22 17:39:23 -0600

    agrege swich

--- VasquezHerbert_Ejercicio2/Program.cs
+++ VasquezHerbert_Ejercicio2/Program.cs
@@ -15,7 +15,19 @@ Console.WriteLine("Trae USB S/N");
 string usb = Console.ReadLine();
 if (hora > 0 && dia > 0 && dia <8 && carnet == "S" || carnet == "N" && autorizacion == "S" || autorizacion == "N")
 {
-
+    switch (opcion)
+    {
+        case 1://Estudiante
+            break;
+        case 2://Docente
+            break;
+        case 3://Tecnico IT
+            break;
+        case 4://Visitante
+            break;
+        default:
+            break;
+    }
 }
 else
 {
