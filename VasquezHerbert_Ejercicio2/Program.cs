b34995b12f9656e01c565e34dc06f27850b04fc6
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-02-22 17:33:02 -0600

    ingreso de datos

--- VasquezHerbert_Ejercicio2/Program.cs
+++ VasquezHerbert_Ejercicio2/Program.cs
@@ -1,2 +1,15 @@
 ﻿//Nuevo proyecto
 //ver si funciona el github
+Console.WriteLine("Ingrese su rol");
+Console.WriteLine("1 Estudiante\r\n2 Docente\r\n3 Técnico IT\r\n4 Visitante");
+int opcion = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el horario");
+int hora = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el dia (1-7)");
+int dia = int.Parse(Console.ReadLine());
+Console.WriteLine("Carnet vigente? S/N");
+String carnet = Console.ReadLine();
+Console.WriteLine("Autorizacion Escrita S/N");
+string autorizacion  = Console.ReadLine();
+Console.WriteLine("Trae USB S/N");
+string usb = Console.ReadLine();
